a94cc784ccf93f6478b6f4b7b1290a7248b06e52
Geoffrey <geo.galagaran@gmail.com>
2017-03-04 22:33:50 +0800

    CHECKOUT.HTML: CHECKOUT.CSS: CHECKOUT.JS: 	Implement design on product-summary and functionality of adding products into the cart

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -1,18 +1,104 @@
-(function() {
-  var down_icon = $('.down-icon')
-  var choices = $('.choices')
+function createCartItems(selected_items, qty) {
+  selected_items.each(function(_,el) {
+    const $el = $(el)
+    const item_name = $el.children('.name').text()
+    const price = $el.children('.price').text()
+    const price_num =  Number(price.match(/\d+/)[0])
+    const total = "₱" + (price_num * qty)
 
-  down_icon.click(function() {
-    console.log(down_icon.hasClass('hide'), choices.hasClass('hide'))
-
-    if(down_icon.hasClass('hide') && !choices.hasClass('hide')) {
-      down_icon.removeClass('hide')
-      choices.addClass('hide')
-    }
-    else if(choices.hasClass('hide') && !down_icon.hasClass('hide')){
-      down_icon.addClass('hide')
-      choices.removeClass('hide')
-    }
+    createItem(item_name, price, qty, total)
+    $el.removeClass('selected-product')
   })
 
+
+  function createItem(item_name, price, qty, total) {
+    const items = $('.items')
+    const item_data = [[item_name, "long prod_name"], [price,"short"], [qty,"short"], [total,"short"]]
+
+    const item = document.createElement('div')
+    item.setAttribute("class", "purchased-item")
+
+    item_data
+    .map(data => {
+      const value = data[0]
+      const span_class = data[1]
+
+      const span = document.createElement('span')
+      const span_val = document.createTextNode(value)
+
+      span.setAttribute("class", span_class)
+      $(span).append(span_val)
+
+      return span
+    })
+    .forEach(span => {
+      $(item).append(span)
+    })
+
+    items.append(item)
+  }
+}
+
+
+(function() {
+  // FILTER SELECT
+  (function() {
+    const $choice = $('.choice')
+
+    function clickEvent() {
+      const $filtered = $('.current-filter')
+      const $current = $(this)
+
+      if(!$current.hasClass('current-filter')) {
+        $filtered.removeClass('current-filter')
+        $current.addClass('current-filter')
+      }
+    }
+    $choice.click(clickEvent)
+  })()
+
+
+  // PRODUCT SELECT
+  ;(function() {
+    const $product = $('.product')
+
+    function clickEvent() {
+      const $self = $(this)
+
+      if($self.hasClass('selected-product')) {
+        $self.removeClass('selected-product')
+      }
+      else {
+        $self.addClass('selected-product')
+      }
+    }
+    $product.click(clickEvent)
+  })()
+
+  // ADD ITEM LIST
+  ;(function() {
+    const $products = $('.products')
+
+    function keyEvent(event) {
+      const button = event.which || event.button
+
+      if(button == 13) {
+        const $selected = $('.selected-product')
+        createCartItems($selected, 2)
+      }
+    }
+
+    function addKeyEvent() {
+      const $body = $("body")
+      $body.keyup(keyEvent)
+    }
+
+    function removeKeyEvent() {
+      const $body = $("body")
+      $body.off("keyup")
+    }
+
+    $products.mouseenter(addKeyEvent)
+    $products.mouseleave(removeKeyEvent)
+  })()
 })()
