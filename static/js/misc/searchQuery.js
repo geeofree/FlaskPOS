6a023395f886960b1d0bb0a78d92f9b68a277ad8
Geoffrey <geo.galagaran@gmail.com>
2017-03-21 18:14:04 +0800

    Cleaned logic on searchQuery.js;

--- static/js/misc/searchQuery.js
+++ static/js/misc/searchQuery.js
@@ -5,6 +5,9 @@ import ajax from "../misc/ajax"
 export default function search(itemELCreator, $wrapper) {
   const $search = $('.search-input')
   const $select = $('.search-select')
+  const $filter = $('.search-filter')
+  const $scope  = $('.search-scope')
+
 
   function resp_callback(resp) {
     resp.forEach(data => $wrapper.append(itemELCreator(data)))
@@ -14,16 +17,14 @@ export default function search(itemELCreator, $wrapper) {
     ajax(query, '/search', callback)
   }
 
-  function searchEvent() {
-    const child = $wrapper.children()
-    child.remove()
-
-    const $filter = $('.search-filter :selected')
-    const $scope  = $('.search-scope :selected')
-
+  function searchEvent(event) {
     const $searchVal = $search.val()
-    const $filterVal = $filter.val()
-    const $scopeVal  = $scope.val()
+    const $filterVal = $filter.find(':selected').val()
+    const $scopeVal  = $scope.find(':selected').val()
+
+    const button = event.which || event.button
+    const keyval = String.fromCharCode(button)
+    const latin  = /\w|\s/.test(keyval)
 
     const search_query = {
       query: $searchVal,
@@ -31,9 +32,22 @@ export default function search(itemELCreator, $wrapper) {
       scope: $scopeVal ? $scopeVal : false
     }
 
-    sendSearchQuery(search_query, resp_callback)
+    if(latin || ((button == 8 || button == 13) && $searchVal == "")) {
+      const child = $wrapper.children()
+      child.remove()
+      sendSearchQuery(search_query, resp_callback)
+    }
+    else {
+      event.preventDefault()
+    }
+
+    if(!button) {
+      const child = $wrapper.children()
+      child.remove()
+      sendSearchQuery(search_query, resp_callback)
+    }
   }
 
-  $select.change(searchEvent)
+  $scope.change(searchEvent)
   $search.keyup(searchEvent)
 }
